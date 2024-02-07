72f297a2a2b65358be2fc656420efc8f15b51f55
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-02-07 06:45:28 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ const { log } = require("fcc-express-bground");
 let app = express();
 
 console.log("Hello World");
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
