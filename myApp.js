1c9d05104566c10b141e41e2fc7fb7afdf7fa103
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-02-07 08:24:10 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,10 +7,10 @@ let app = express();
 console.log("Hello World");
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
-  bodyParser.urlencoded({ extended: false });
+
   next();
 });
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
