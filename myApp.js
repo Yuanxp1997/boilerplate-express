bd571f05eb614ed4195d48afdc95b05ca652e80a
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-02-07 08:00:11 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -32,4 +32,7 @@ app.get(
 app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
+app.route("/name").get((req, res) => {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+});
 module.exports = app;
